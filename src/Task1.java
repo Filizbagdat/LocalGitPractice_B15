b0fe8f13f69bc968ee9cb6ba2534deb5cca68028
Filiz Bagdatli <melihahalim@hotmail.de>
2024-03-21 00:27:49 +0100

    git commands are not difficult

--- src/Task1.java
+++ src/Task1.java
@@ -1,5 +1,10 @@
 public class Task1 {
     public static void main(String[] args) {
         System.out.println("I am learning git commands");
+        System.out.println("I am learning git add");
+        System.out.println("I am learning git status ");
+        System.out.println("I am learning git log ");
+        System.out.println("I am learning git commit -m ");
+        System.out.println("I am learning git add . ");
     }
 }
