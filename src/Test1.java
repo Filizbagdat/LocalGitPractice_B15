afb14a6baeda580c43c0d5e5ed3a2d1ef0492ccc
filiz <melihahalim@hotmai.de>
2024-03-20 21:53:03 +0100

    new

--- src/Test1.java
+++ src/Test1.java
@@ -1,5 +1,9 @@
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("Test1");
+        System.out.println("Test11");
+        System.out.println("Test111");
+        System.out.println("Test1111");
+        System.out.println("Test11111");
     }
 }
