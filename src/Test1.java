4c5d69eb8c3ca181043bea05473bee8130272b4d
Filiz Bagdatli <melihahalim@hotmail.de>
2024-03-21 21:56:36 +0100

    Test1 file  in Filiz1 uploaded

--- src/Test1.java
+++ src/Test1.java
@@ -1,7 +1,7 @@
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("localTest1");
-        System.out.println("Test11");
+        System.out.println("local  my Test11");
         System.out.println("Test111");
         System.out.println("Test1111");
         System.out.println("Test11111");
